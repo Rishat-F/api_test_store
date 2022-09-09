826c8cdd1fff516bb588da20d6403218d82b3495
yavv951 <vadimv95189@gmail.com>
2022-09-09 16:24:45 +0300

    add tests register fix

--- tests/test_register/test_register.py
+++ tests/test_register/test_register.py
@@ -1,7 +1,7 @@
 from endpoints_models.register.model import DataRegister, ResponseRegister
 
 
-def test_true(app) -> None:
+def test_register(app) -> None:
     """"""
     data = DataRegister.random()
     response = app.register.post(data=data, type_response=ResponseRegister)
